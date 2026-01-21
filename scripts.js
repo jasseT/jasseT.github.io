95f53419aec914d4d0888fafa04ecd88f79fe249
jasse <mail@mail.com>
2026-01-21 13:42:53 +0200

    some minor changes

--- scripts.js
+++ scripts.js
@@ -2,14 +2,14 @@ const boxes = document.querySelectorAll('.left-column .box');
 const contents = document.querySelectorAll('.right .content');
 
 boxes.forEach(box => {
-	
+
 	function showContent() {
-		// Hide all contents
-		contents.forEach(content => {
-			content.classList.remove('active'); // Remove active class for fade out
-			content.style.display = 'none'; // Hide immediately to reset for fade in
-		});
-		
+        // Hide all contents
+        contents.forEach(content => {
+            content.classList.remove('active'); // Remove active class for fade out
+            content.style.display = 'none'; // Hide immediately to reset for fade in
+        });
+
 		// Show the corresponding content
 		const contentClass = box.id + '-content';
 		const activeContent = document.querySelector(`.${contentClass}`);
@@ -20,12 +20,12 @@ boxes.forEach(box => {
 			}, 10); // Small timeout to allow for display
 		}
 	}
-	
+
 	// Mouse hover listener for computers
 	box.addEventListener('mouseenter', () => {
 		showContent();
 	});
-	
+
 	// Touch listener for large touch screens, small screens have a separate menu
 	box.addEventListener('touchstart', () => {
 		event.preventDefault();
